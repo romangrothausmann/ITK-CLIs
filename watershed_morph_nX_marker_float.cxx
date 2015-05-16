7cfcedab2eab7af5d3b03a5e0d7900c336561549
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2015-05-16 19:48:42 +0200

    basic implementation of watershed_morph_nX_marker_float.cxx based on watershed_morph_nX_marker_UI8.cxx

--- watershed_morph_nX_marker_float.cxx
+++ watershed_morph_nX_marker_float.cxx
@@ -1,54 +1,64 @@
-////program for
-//01: based on template_2inputs.cxx
+////program for itkMorphologicalWatershedFromMarkersImageFilter
+//01: based on template_2inputs.cxx and watershed_morph_nX_marker_UI8.cxx
 
 
-#include <complex>
+#include <proc/readproc.h>//for look_up_our_self
+#include <unistd.h>//for sysconf
 
-#include "itkFilterWatcher.h"
 #include <itkImageFileReader.h>
 #include <itkImageFileWriter.h>
 
+#include <itkCommand.h>
+
+#include <itkShiftScaleImageFilter.h>
+#include <itkStatisticsImageFilter.h>
+#include <itkMorphologicalWatershedFromMarkersImageFilter.h>
+#include <itkBinaryThresholdImageFilter.h>
+#include <itkAddImageFilter.h>
+#include <itkGradientMagnitudeImageFilter.h>
+#include <itkChangeLabelImageFilter.h>
+#include <itkMaskImageFilter.h>
 
 
-// template<typename InputImageType, typename OutputImageType>
-// void FilterEventHandlerITK(itk::Object *caller, const itk::EventObject &event, void*){
 
-//     const itk::ProcessObject* filter = static_cast<const itk::ProcessObject*>(caller);
+template<typename InputImageType>
+void FilterEventHandlerITK(itk::Object *caller, const itk::EventObject &event, void*){
 
-//     if(itk::ProgressEvent().CheckEvent(&event))
-//         fprintf(stderr, "\r%s progress: %5.1f%%", filter->GetNameOfClass(), 100.0 * filter->GetProgress());//stderr is flushed directly
-//     else if(itk::IterationEvent().CheckEvent(&event))
-//         std::cerr << " Iteration: " << (dynamic_cast<itk::SliceBySliceImageFilter<InputImageType, OutputImageType> *>(caller))->GetSliceIndex() << std::endl;
-//     else if(strstr(filter->GetNameOfClass(), "ImageFileReader"))
-//         std::cerr << "Reading: " << (dynamic_cast<itk::ImageFileReader<InputImageType> *>(caller))->GetFileName() << std::endl;
-//     else if(itk::EndEvent().CheckEvent(&event))
-//         std::cerr << std::endl;
-//     }
+    const itk::ProcessObject* filter = static_cast<const itk::ProcessObject*>(caller);
+
+    if(itk::ProgressEvent().CheckEvent(&event))
+        fprintf(stderr, "\r%s progress: %5.1f%%", filter->GetNameOfClass(), 100.0 * filter->GetProgress());//stderr is flushed directly
+    else if(strstr(filter->GetNameOfClass(), "ImageFileReader"))
+        std::cerr << "Reading: " << (dynamic_cast<itk::ImageFileReader<InputImageType> *>(caller))->GetFileName() << std::endl;
+    else if(itk::EndEvent().CheckEvent(&event))
+        std::cerr << std::endl;
+    }
 
 
 
 template<typename InputComponent1, typename TypeInputComponentType2, typename InputPixelType1, typename InputPixelType2, size_t Dimension>
 int DoIt(int argc, char *argv[]){
 
-    typedef   OutputPixelType;
-
     typedef itk::Image<InputPixelType1, Dimension>  InputImageType1;
-    typedef itk::Image<InputPixelType2, Dimension>  InputImageType2;
-    typedef itk::Image<OutputPixelType, Dimension>  OutputImageType;
+    typedef itk::Image<InputPixelType2, Dimension>  LabelImageType;
+    typedef itk::Image<float, Dimension>            GreyImageType;
 
-    // itk::CStyleCommand::Pointer eventCallbackITK;
-    // eventCallbackITK = itk::CStyleCommand::New();
-    // eventCallbackITK->SetCallback(FilterEventHandlerITK<InputImageType, OutputImageType>);
+    itk::CStyleCommand::Pointer eventCallbackITK;
+    eventCallbackITK = itk::CStyleCommand::New();
+    eventCallbackITK->SetCallback(FilterEventHandlerITK<InputImageType1>);
+
+    ////for mem monitoring: http://stackoverflow.com/questions/669438/how-to-get-memory-usage-at-run-time-in-c
+    struct proc_t usage;//description in: /usr/include/proc/readproc.h
+    double page_size_mb = sysconf(_SC_PAGE_SIZE) / 1024. / 1024.; // in case x86-64 is configured to use 2MB pages
 
 
+    typename GreyImageType::Pointer input;
+        {
     typedef itk::ImageFileReader<InputImageType1> ReaderType1;
     typename ReaderType1::Pointer reader1 = ReaderType1::New();
 
     reader1->SetFileName(argv[1]);
-    reader1->ReleaseDataFlagOn();
-    FilterWatcher watcherI1(reader1);
-    watcherI1.QuietOn();
-    watcherI1.ReportTimeOn();
+    reader1->AddObserver(itk::AnyEvent(), eventCallbackITK);
     try{
         reader1->Update();
         }
@@ -57,17 +67,35 @@ int DoIt(int argc, char *argv[]){
         return EXIT_FAILURE;
         }
 
-    const typename InputImageType1::Pointer& input1= reader1->GetOutput();
+        typedef itk::ShiftScaleImageFilter<InputImageType1, GreyImageType> SSType;
+        typename SSType::Pointer ss = SSType::New();
+        if(atoi(argv[6]))
+            ss->SetScale(-1); //invert by mul. with -1
+        else
+            ss->SetScale(1); //just convert to GreyImageType
+        ss->SetInput(reader1->GetOutput());
+        ss->AddObserver(itk::ProgressEvent(), eventCallbackITK);
+        ss->AddObserver(itk::EndEvent(), eventCallbackITK);
+        try{
+            ss->Update();
+            }
+        catch(itk::ExceptionObject &ex){
+            std::cerr << ex << std::endl;
+            return EXIT_FAILURE;
+            }
+        input= ss->GetOutput();
+        input->DisconnectPipeline();//will need its own Delete later on!
+        }
+    look_up_our_self(&usage); printf("vsize: %.3f mb; rss: %.3f mb\n", usage.vsize/1024./1024., usage.rss * page_size_mb);
 
-
-    typedef itk::ImageFileReader<InputImageType2> ReaderType2;
+    typename LabelImageType::Pointer labelImg;
+    typename LabelImageType::PixelType labelCnt;
+        {
+    typedef itk::ImageFileReader<LabelImageType> ReaderType2;
     typename ReaderType2::Pointer reader2 = ReaderType2::New();
 
     reader2->SetFileName(argv[2]);
-    reader2->ReleaseDataFlagOn();
-    FilterWatcher watcherI2(reader2);
-    watcherI2.QuietOn();
-    watcherI2.ReportTimeOn();
+    reader2->AddObserver(itk::AnyEvent(), eventCallbackITK);
     try{
         reader2->Update();
         }
@@ -76,39 +104,148 @@ int DoIt(int argc, char *argv[]){
         return EXIT_FAILURE;
         }
 
-    const typename InputImageType2::Pointer& input2= reader2->GetOutput();
+        typedef itk::StatisticsImageFilter<LabelImageType> FilterType;
+        typename FilterType::Pointer stat= FilterType::New();
+        stat->SetInput(reader2->GetOutput());
+        //stat->InPlaceOn();//not available
+        stat->ReleaseDataFlagOn();
 
+        stat->AddObserver(itk::ProgressEvent(), eventCallbackITK);
+        stat->AddObserver(itk::EndEvent(), eventCallbackITK);
+        try{
+            stat->Update();
+            }
+        catch(itk::ExceptionObject &ex){
+            std::cerr << ex << std::endl;
+            return EXIT_FAILURE;
+            }
 
-    typedef itk::<InputImageType1, InputImageType2, OutputImageType> FilterType;
-    typename FilterType::Pointer filter= FilterType::New();
-    filter->SetInput1(reader1->GetOutput());
-    filter->SetInput2(reader2->GetOutput());
-    filter->ReleaseDataFlagOn();
-    filter->InPlaceOn();
+        std::cerr << "Min: " << +stat->GetMinimum() << " Max: " << +stat->GetMaximum() << " Mean: " << +stat->GetMean() << " Std: " << +stat->GetSigma() << " Variance: " << +stat->GetVariance() << " Sum: " << +stat->GetSum() << std::endl;
 
-    FilterWatcher watcher1(filter);
-    // filter->AddObserver(itk::ProgressEvent(), eventCallbackITK);
-    // filter->AddObserver(itk::IterationEvent(), eventCallbackITK);
-    // filter->AddObserver(itk::EndEvent(), eventCallbackITK);
-    try{
-        filter->Update();
+        labelCnt= stat->GetMaximum();
+        labelImg= stat->GetOutput();
+        labelImg->DisconnectPipeline();//will need its own Delete later on!
         }
-    catch(itk::ExceptionObject &ex){
-        std::cerr << ex << std::endl;
-        return EXIT_FAILURE;
+    look_up_our_self(&usage); printf("vsize: %.3f mb; rss: %.3f mb\n", usage.vsize/1024./1024., usage.rss * page_size_mb);
+
+    bool ws0_conn= true;//true reduces amount of watersheds
+    bool ws_conn= false;
+
+    uint8_t NumberOfExtraWS= atoi(argv[5]);
+
+    typename LabelImageType::Pointer markerImg;
+    typename LabelImageType::Pointer borderImg;
+    typename GreyImageType::Pointer gradientImg;
+
+
+    typedef itk::MorphologicalWatershedFromMarkersImageFilter<GreyImageType, LabelImageType> MWatershedType;
+    typename MWatershedType::Pointer ws = MWatershedType::New();
+    ws->SetMarkWatershedLine(NumberOfExtraWS); //use borders if higher order WS are wanted
+    ws->SetFullyConnected(ws0_conn);
+    ws->SetInput(input);
+    ws->SetMarkerImage(labelImg);
+    ws->AddObserver(itk::ProgressEvent(), eventCallbackITK);
+    ws->AddObserver(itk::EndEvent(), eventCallbackITK);
+    ws->Update();
+
+    look_up_our_self(&usage); printf("vsize: %.3f mb; rss: %.3f mb\n", usage.vsize/1024./1024., usage.rss * page_size_mb);
+    if(NumberOfExtraWS > 0){
+
+            {//scoped for better consistency
+            // extract the watershed lines and combine with the orginal markers
+            typedef itk::BinaryThresholdImageFilter<LabelImageType, LabelImageType> ThreshType;
+            typename ThreshType::Pointer th = ThreshType::New();
+            th->SetUpperThreshold(0);
+            th->SetOutsideValue(0);
+            // set the inside value to the number of markers + 1
+            th->SetInsideValue(labelCnt + 1);
+            th->SetInput(ws->GetOutput());
+            th->Update();
+            borderImg= th->GetOutput();
+            borderImg->DisconnectPipeline();
+            }
+
+        look_up_our_self(&usage); printf("vsize: %.3f mb; rss: %.3f mb\n", usage.vsize/1024./1024., usage.rss * page_size_mb);
+        // to combine the markers again
+        typedef itk::AddImageFilter<LabelImageType, LabelImageType, LabelImageType> AddType;
+        typename AddType::Pointer adder = AddType::New();
+        adder->InPlaceOn();
+        adder->ReleaseDataFlagOn();
+
+        // to create gradient magnitude image
+        typedef itk::GradientMagnitudeImageFilter<GreyImageType, GreyImageType> GMType;
+        typename GMType::Pointer gm = GMType::New();
+
+        gm->AddObserver(itk::ProgressEvent(), eventCallbackITK);
+        gm->AddObserver(itk::EndEvent(), eventCallbackITK);
+        //gm->InPlaceOn();//not available
+        //gm->ReleaseDataFlagOn();//gm output is used for ws and next gm!
+        gradientImg= input;
+        input->ReleaseDataFlagOn();//free input as soon as gradientImg has been used as input by gm
+
+        ws->SetMarkWatershedLine(false); //no use for a border in higher stages
+        ws->SetFullyConnected(ws_conn);
+        //ws->InPlaceOn();//not available
+        ws->ReleaseDataFlagOn();
+
+        // to delete the background label
+        typedef itk::ChangeLabelImageFilter<LabelImageType, LabelImageType> ChangeLabType;
+        typename ChangeLabType::Pointer ch= ChangeLabType::New();
+        ch->SetChange(labelCnt + 1, 0);
+        ch->InPlaceOn();
+        ch->ReleaseDataFlagOn();//will be handled by adder if adder->InPlaceOn() AND used for adder->SetInput1
+
+        std::cerr << "Starting extra runs..." << std::endl;
+
+        for(char i= 0; i < NumberOfExtraWS; i++){
+            //// DisconnectPipeline() on outputs does not make sense here becauses:
+            //// - the filter it belongs to is not scoped to the loop
+            //// - it likely avoids mem freeing expected from ReleaseDataFlagOn()
+
+            // Add the marker image to the watershed line image
+            //// labelImg will not be needed again afterwards
+            //// so setting Input1 to labelImg with InPlaceOn() will overwrite labelImg
+            //// with InPlaceOn() use Input2 for borderImg to avoid loosing orig borderImg
+            adder->SetInput1(labelImg);//if InPlaceOn(), input1 will be changed! and used as output!
+            adder->SetInput2(borderImg);
+            adder->Update();//frees mem of labelImg if ch->ReleaseDataFlagOn(); even if adder->InPlaceOn();?
+            markerImg= adder->GetOutput();
+            //markerImg->DisconnectPipeline();
+            //labelImg->Delete();//free mem of labelImg originating from initial markers; do not use if adder->InPlaceOn()
+
+            look_up_our_self(&usage); printf("vsize: %.3f mb; rss: %.3f mb\n", usage.vsize/1024./1024., usage.rss * page_size_mb);
+            // compute a gradient
+            gm->SetInput(gradientImg);
+            gm->Update();
+            //gradientImg->Delete();//free mem of orig input / last gradientImg <- bad! causes double free! use ReleaseDataFlag on reader instead and rely on smart ponter logic for last gm-output? http://public.kitware.com/pipermail/insight-users/2009-October/033004.html
+            gradientImg= gm->GetOutput();
+            gradientImg->DisconnectPipeline();
+
+            look_up_our_self(&usage); printf("vsize: %.3f mb; rss: %.3f mb\n", usage.vsize/1024./1024., usage.rss * page_size_mb);
+            // Now apply higher order watershed
+            ws->SetInput(gradientImg);
+            ws->SetMarkerImage(markerImg);
+            ws->Update();//frees mem of markerImg if adder->ReleaseDataFlagOn();
+
+            look_up_our_self(&usage); printf("vsize: %.3f mb; rss: %.3f mb\n", usage.vsize/1024./1024., usage.rss * page_size_mb);
+            // delete the background label
+            ch->SetInput(ws->GetOutput());//with ch->InPlaceOn() ws output will be overwritten!
+            ch->Update();//frees mem of ws output if ws->ReleaseDataFlagOn();
+            labelImg= ch->GetOutput();
+            //labelImg->DisconnectPipeline();
+            }
         }
+    else
+        labelImg= ws->GetOutput();
 
-
-    const typename OutputImageType::Pointer& output= filterXYZ->GetOutput();
-
-    typedef itk::ImageFileWriter<OutputImageType>  WriterType;
+    typedef itk::ImageFileWriter<LabelImageType>  WriterType;
     typename WriterType::Pointer writer = WriterType::New();
 
-    FilterWatcher watcherO(writer);
     writer->SetFileName(argv[3]);
-    writer->SetInput(output);
-    //writer->UseCompressionOn();
-    //writer->SetUseCompression(atoi(argv[4]));
+    writer->SetInput(labelImg);
+    writer->SetUseCompression(atoi(argv[4]));
+    writer->AddObserver(itk::ProgressEvent(), eventCallbackITK);
+    writer->AddObserver(itk::EndEvent(), eventCallbackITK);
     try{
         writer->Update();
         }
@@ -154,22 +291,6 @@ int dispatch_pT2(itk::ImageIOBase::IOPixelType pixelType2, size_t dimensionType,
         typedef InputComponentType2 InputPixelType2;
         res= dispatch_D<InputComponentType1, InputComponentType2, InputPixelType1, InputPixelType2>(dimensionType, argc, argv);
         } break;
-    case itk::ImageIOBase::RGB:{
-        typedef itk::RGBPixel<InputComponentType2> InputPixelType2;
-        res= dispatch_D<InputComponentType1, InputComponentType2, InputPixelType1, InputPixelType2>(dimensionType, argc, argv);
-        } break;
-    case itk::ImageIOBase::RGBA:{
-        typedef itk::RGBAPixel<InputComponentType2> InputPixelType2;
-        res= dispatch_D<InputComponentType1, InputComponentType2, InputPixelType1, InputPixelType2>(dimensionType, argc, argv);
-        } break;
-    case itk::ImageIOBase::COMPLEX:{
-        typedef std::complex<InputComponentType2> InputPixelType2;
-        res= dispatch_D<InputComponentType1, InputComponentType2, InputPixelType1, InputPixelType2>(dimensionType, argc, argv);
-        } break;
-    case itk::ImageIOBase::VECTOR:{
-        typedef itk::VariableLengthVector<InputComponentType2> InputPixelType2;
-        res= dispatch_D<InputComponentType1, InputComponentType2, InputPixelType1, InputPixelType2>(dimensionType, argc, argv);
-        } break;
     case itk::ImageIOBase::UNKNOWNPIXELTYPE:
     default:
         std::cerr << std::endl << "Error: Pixel type not handled!" << std::endl;
@@ -188,22 +309,6 @@ int dispatch_pT1(itk::ImageIOBase::IOPixelType pixelType1, itk::ImageIOBase::IOP
     switch (pixelType1){
     case itk::ImageIOBase::SCALAR:{
         typedef InputComponentType1 InputPixelType1;
-        res= dispatch_pT2<InputComponentType1, InputComponentType2, InputPixelType1>(pixelType2, dimensionType, argc, argv);
-        } break;
-    case itk::ImageIOBase::RGB:{
-        typedef itk::RGBPixel<InputComponentType1> InputPixelType1;
-        res= dispatch_pT2<InputComponentType1, InputComponentType2, InputPixelType1>(pixelType2, dimensionType, argc, argv);
-        } break;
-    case itk::ImageIOBase::RGBA:{
-        typedef itk::RGBAPixel<InputComponentType1> InputPixelType1;
-        res= dispatch_pT2<InputComponentType1, InputComponentType2, InputPixelType1>(pixelType2, dimensionType, argc, argv);
-        } break;
-    case itk::ImageIOBase::COMPLEX:{
-        typedef std::complex<InputComponentType1> InputPixelType1;
-        res= dispatch_pT2<InputComponentType1, InputComponentType2, InputPixelType1>(pixelType2, dimensionType, argc, argv);
-        } break;
-    case itk::ImageIOBase::VECTOR:{
-        typedef itk::VariableLengthVector<InputComponentType1> InputPixelType1;
         res= dispatch_pT2<InputComponentType1, InputComponentType2, InputPixelType1>(pixelType2, dimensionType, argc, argv);
         } break;
     case itk::ImageIOBase::UNKNOWNPIXELTYPE:
@@ -253,14 +358,6 @@ int dispatch_cT2(itk::ImageIOBase::IOComponentType componentType2, itk::ImageIOB
         } break;
     case itk::ImageIOBase::LONG:{         // int64_t
         typedef long InputComponentType2;
-        res= dispatch_pT1<InputComponentType1, InputComponentType2>(pixelType1, pixelType2, dimensionType, argc, argv);
-        } break;
-    case itk::ImageIOBase::FLOAT:{        // float32
-        typedef float InputComponentType2;
-        res= dispatch_pT1<InputComponentType1, InputComponentType2>(pixelType1, pixelType2, dimensionType, argc, argv);
-        } break;
-    case itk::ImageIOBase::DOUBLE:{       // float64
-        typedef double InputComponentType2;
         res= dispatch_pT1<InputComponentType1, InputComponentType2>(pixelType1, pixelType2, dimensionType, argc, argv);
         } break;
     case itk::ImageIOBase::UNKNOWNCOMPONENTTYPE:
@@ -358,13 +455,14 @@ void GetImageType (std::string fileName,
 
 
 int main(int argc, char *argv[]){
-    if ( argc != 5 ){
+    if ( argc != 7 ){
         std::cerr << "Missing Parameters: "
                   << argv[0]
-                  << " Input_Image1"
-                  << " Input_Image2"
+                  << " Input_Image"
+                  << " Marker_Image"
                   << " Output_Image"
                   << " compress"
+                  << " NumberOfExtraWS invert"
                   << std::endl;
 
         return EXIT_FAILURE;
