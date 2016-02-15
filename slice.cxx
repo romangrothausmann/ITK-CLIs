e32202b252694b2e55968d2baf98bcf131a90c81
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2016-02-15 16:15:44 +0100

    basic implementation of slice.cxx

--- slice.cxx
+++ slice.cxx
@@ -1,4 +1,4 @@
-////program for
+////program to extract a slice
 //01: based on template.cxx
 
 
@@ -6,38 +6,18 @@
 
 #include "itkFilterWatcher.h"
 #include <itkImageFileReader.h>
+#include <itkExtractImageFilter.h>
 #include <itkImageFileWriter.h>
-
-
-
-// template<typename ReaderImageType, typename WriterImageType>
-// void FilterEventHandlerITK(itk::Object *caller, const itk::EventObject &event, void*){
-
-//     const itk::ProcessObject* filter = static_cast<const itk::ProcessObject*>(caller);
-
-//     if(itk::ProgressEvent().CheckEvent(&event))
-//         fprintf(stderr, "\r%s progress: %5.1f%%", filter->GetNameOfClass(), 100.0 * filter->GetProgress());//stderr is flushed directly
-//     else if(itk::StartEvent().CheckEvent(&event)){
-// 	if(strstr(filter->GetNameOfClass(), "ImageFileReader"))
-// 	    std::cerr << "Reading: " << (dynamic_cast<itk::ImageFileReader<ReaderImageType> *>(caller))->GetFileName() << std::endl;//cast only works if reader was instanciated for ReaderImageType!
-// 	else if(strstr(filter->GetNameOfClass(), "ImageFileWriter"))
-// 	    std::cerr << "Writing: " << (dynamic_cast<itk::ImageFileWriter<WriterImageType> *>(caller))->GetFileName() << std::endl;//cast only works if writer was instanciated for WriterImageType!
-// 	}
-//     else if(itk::IterationEvent().CheckEvent(&event))
-//         std::cerr << " Iteration: " << (dynamic_cast<itk::SliceBySliceImageFilter<ReaderImageType, WriterImageType> *>(caller))->GetSliceIndex() << std::endl;
-//     else if(itk::EndEvent().CheckEvent(&event))
-//         std::cerr << std::endl;
-//     }
 
 
 
 template<typename InputComponentType, typename InputPixelType, size_t Dimension>
 int DoIt(int argc, char *argv[]){
 
-    typedef   OutputPixelType;
+    typedef InputPixelType OutputPixelType;
 
     typedef itk::Image<InputPixelType, Dimension>  InputImageType;
-    typedef itk::Image<OutputPixelType, Dimension>  OutputImageType;
+    typedef itk::Image<OutputPixelType, Dimension-1>  OutputImageType;
 
     // itk::CStyleCommand::Pointer eventCallbackITK;
     // eventCallbackITK = itk::CStyleCommand::New();
@@ -62,18 +42,46 @@ int DoIt(int argc, char *argv[]){
 
     const typename InputImageType::Pointer& input= reader->GetOutput();
 
+    unsigned int axis;
+    switch(argv[4][0]){ // strcasestr(argv[3],"x"):
+    case 'x': 
+    case 'X':
+	axis= 0;
+	break;
+    case 'y': 
+    case 'Y':
+	axis= 1;
+	break;
+    case 'z': 
+    case 'Z':
+	axis= 2;
+	break;
+    default: 
+	std::cerr << "Warning: Axis specification not known. Interpreting as numeric specification!" << std::endl;
+	axis= atoi(argv[4]);
+	break;
+	}//switch 
 
 
-    typedef itk::<InputImageType> FilterType;
+    typedef itk::ExtractImageFilter<InputImageType,OutputImageType> FilterType;
     typename FilterType::Pointer filter= FilterType::New();
     filter->SetInput(input);
+    filter->SetDirectionCollapseToSubmatrix(); // SetDirectionCollapseToIdentity();
+
+    typename FilterType::InputImageRegionType slice(input->GetLargestPossibleRegion());
+    slice.SetSize(axis, 0);
+    slice.SetIndex(axis, atoi(argv[5]));
+
+    std::cerr << "slice region index: " << slice.GetIndex()
+	      << "  size: " << slice.GetSize()
+	      << std::endl;
+
+
+    filter->SetExtractionRegion(slice);
     filter->ReleaseDataFlagOn();
     filter->InPlaceOn();
 
     FilterWatcher watcher1(filter);
-    // filter->AddObserver(itk::ProgressEvent(), eventCallbackITK);
-    // filter->AddObserver(itk::IterationEvent(), eventCallbackITK);
-    // filter->AddObserver(itk::EndEvent(), eventCallbackITK);
     try{
         filter->Update();
         }
@@ -83,7 +91,7 @@ int DoIt(int argc, char *argv[]){
         }
 
 
-    const typename OutputImageType::Pointer& output= filterXYZ->GetOutput();
+    const typename OutputImageType::Pointer& output= filter->GetOutput();
 
     typedef itk::ImageFileWriter<OutputImageType>  WriterType;
     typename WriterType::Pointer writer = WriterType::New();
@@ -91,8 +99,7 @@ int DoIt(int argc, char *argv[]){
     FilterWatcher watcherO(writer);
     writer->SetFileName(argv[2]);
     writer->SetInput(output);
-    //writer->UseCompressionOn();
-    //writer->SetUseCompression(atoi(argv[3]));
+    writer->SetUseCompression(atoi(argv[3]));
     try{
         writer->Update();
         }
@@ -110,9 +117,6 @@ template<typename InputComponentType, typename InputPixelType>
 int dispatch_D(size_t dimensionType, int argc, char *argv[]){
     int res= EXIT_FAILURE;
     switch (dimensionType){
-    case 1:
-        res= DoIt<InputComponentType, InputPixelType, 1>(argc, argv);
-        break;
     case 2:
         res= DoIt<InputComponentType, InputPixelType, 2>(argc, argv);
         break;
@@ -249,12 +253,13 @@ void GetImageType (std::string fileName,
 
 
 int main(int argc, char *argv[]){
-    if ( argc != 4 ){
+    if ( argc != 6 ){
         std::cerr << "Missing Parameters: "
                   << argv[0]
                   << " Input_Image"
                   << " Output_Image"
                   << " compress"
+                  << " axis slice#"
                   << std::endl;
 
         return EXIT_FAILURE;
