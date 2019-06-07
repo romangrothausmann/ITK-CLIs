05b4a38be97b7859dbfb023f967362c1cb0306e7
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2019-06-07 16:02:22 +0200

    exampel to debug Gauss interpolator when resampling with streaming:
    from: https://github.com/InsightSoftwareConsortium/ITK/pull/998#issuecomment-499882759

---
+++ test.cxx
@@ -0,0 +1,56 @@
+#include <itkImageFileReader.h>
+#include <itkResampleImageFilter.h>
+#include <itkGaussianInterpolateImageFunction.h>
+#include <itkImageFileWriter.h>
+
+using ImageType = itk::Image<int, 3>;
+using ReaderType = itk::ImageFileReader<ImageType>;
+using FilterType = itk::ResampleImageFilter<ImageType, ImageType>;
+using TransformType = itk::IdentityTransform<double, 3>;
+using InterpolatorType = itk::GaussianInterpolateImageFunction<ImageType>;
+using WriterType = itk::ImageFileWriter<ImageType>;
+
+int main(int argc, char** argv)
+{
+    ReaderType::Pointer reader = ReaderType::New();
+    reader->SetFileName(argv[1]);
+    reader->UpdateOutputInformation();
+    ImageType::Pointer input = reader->GetOutput();
+
+    typename TransformType::Pointer transform = TransformType::New();
+    transform->SetIdentity();
+    typename ImageType::SpacingType outputSpacing;
+    outputSpacing.Fill(1.0);
+
+    FilterType::Pointer filter = FilterType::New();
+    filter->SetInput(reader->GetOutput());
+    filter->SetInterpolator(InterpolatorType::New());
+    filter->SetSize(input->GetLargestPossibleRegion().GetSize());
+    filter->SetOutputOrigin(input->GetOrigin());
+
+    filter->SetTransform(transform);
+    filter->SetOutputSpacing(outputSpacing);
+    filter->SetOutputDirection(input->GetDirection());
+    filter->SetDefaultPixelValue(0);
+    filter->ReleaseDataFlagOn();
+
+    WriterType::Pointer writer = WriterType::New();
+    writer->SetInput(filter->GetOutput());
+    try
+    {
+        writer->SetFileName("C:/Misc/Tester/out1.mha");
+        writer->SetNumberOfStreamDivisions(1);
+        writer->Update();
+
+        writer->SetFileName("C:/Misc/Tester/out4.mha");
+        writer->SetNumberOfStreamDivisions(4);
+        writer->Update();
+    }
+    catch (itk::ExceptionObject& exc)
+    {
+        std::cout << exc;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
