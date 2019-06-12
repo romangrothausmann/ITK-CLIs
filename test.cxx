fe6e1a5dfa96651f888df756753062bf12e05e43
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2019-06-12 16:34:41 +0200

    use input->Modified();

--- test.cxx
+++ test.cxx
@@ -61,6 +61,7 @@ int main(int argc, char** argv)
         writer->SetNumberOfStreamDivisions(1);
         writer->Update();
 
+	input->Modified();
         writer->SetFileName("out10.mhd");
         writer->SetNumberOfStreamDivisions(10);
         writer->Update();
