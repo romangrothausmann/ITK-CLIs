0e444e6cf553b025c00ce81be4eb275e2d11f53d
Roman Grothausmann <romangrothausmann@users.noreply.github.com>
2019-11-05 10:01:48 +0100

    basic implementation of shift-scale_window

--- shift-scale_window.cxx
+++ shift-scale_window.cxx
@@ -1,4 +1,4 @@
-////program for
+////program to scale image values to a lower bit-depth
 //01: based on template.cxx
 
 
@@ -6,42 +6,25 @@
 
 #include "itkFilterWatcher.h"
 #include <itkImageFileReader.h>
+#include <itkIntensityWindowingImageFilter.h>
+#include <itkCastImageFilter.h>
 #include <itkImageFileWriter.h>
-
-
-
-// template<typename ReaderImageType, typename WriterImageType>
-// void FilterEventHandlerITK(itk::Object *caller, const itk::EventObject &event, void*){
-
-//     const itk::ProcessObject* filter = static_cast<const itk::ProcessObject*>(caller);
-
-//     if(itk::ProgressEvent().CheckEvent(&event))
-//         fprintf(stderr, "\r%s progress: %5.1f%%", filter->GetNameOfClass(), 100.0 * filter->GetProgress());//stderr is flushed directly
-//     else if(itk::StartEvent().CheckEvent(&event)){
-// 	if(strstr(filter->GetNameOfClass(), "ImageFileReader"))
-// 	    std::cerr << "Reading: " << (dynamic_cast<itk::ImageFileReader<ReaderImageType> *>(caller))->GetFileName() << std::endl;//cast only works if reader was instanciated for ReaderImageType!
-// 	else if(strstr(filter->GetNameOfClass(), "ImageFileWriter"))
-// 	    std::cerr << "Writing: " << (dynamic_cast<itk::ImageFileWriter<WriterImageType> *>(caller))->GetFileName() << std::endl;//cast only works if writer was instanciated for WriterImageType!
-// 	}
-//     else if(itk::IterationEvent().CheckEvent(&event))
-//         std::cerr << " Iteration: " << (dynamic_cast<itk::SliceBySliceImageFilter<ReaderImageType, WriterImageType> *>(caller))->GetSliceIndex() << std::endl;
-//     else if(itk::EndEvent().CheckEvent(&event))
-//         std::cerr << std::endl;
-//     }
 
 
 
 template<typename InputComponentType, typename InputPixelType, size_t Dimension>
 int DoIt(int argc, char *argv[]){
 
-    typedef   OutputPixelType;
+#ifdef USE_UI8
+    typedef uint8_t  OutputPixelType;
+    std::cerr << "Using UInt8." << std::endl;
+#else
+    typedef uint16_t  OutputPixelType;
+    std::cerr << "Using UInt16." << std::endl;
+#endif
 
     typedef itk::Image<InputPixelType, Dimension>  InputImageType;
     typedef itk::Image<OutputPixelType, Dimension>  OutputImageType;
-
-    // itk::CStyleCommand::Pointer eventCallbackITK;
-    // eventCallbackITK = itk::CStyleCommand::New();
-    // eventCallbackITK->SetCallback(FilterEventHandlerITK<InputImageType, OutputImageType>);
 
     int CompChunk= atoi(argv[3]);
     bool noSDI= CompChunk <= 1; // SDI only if CompChunk > 1
@@ -71,17 +54,24 @@ int DoIt(int argc, char *argv[]){
 
 
 
-    typedef itk::<InputImageType> FilterType;
+    typedef itk::IntensityWindowingImageFilter<InputImageType> FilterType;
     typename FilterType::Pointer filter= FilterType::New();
     filter->SetInput(input);
+    filter->SetWindowMinimum(static_cast<InputPixelType>(atof(argv[4])));
+    filter->SetWindowMaximum(static_cast<InputPixelType>(atof(argv[5])));
+    filter->SetOutputMinimum(itk::NumericTraits<OutputPixelType>::min());
+    filter->SetOutputMaximum(itk::NumericTraits<OutputPixelType>::max());
     filter->ReleaseDataFlagOn();
     filter->InPlaceOn();
 
+    typedef itk::CastImageFilter<InputImageType, OutputImageType> CastFilterType;
+    typename CastFilterType::Pointer  caster=  CastFilterType::New();
+    caster->SetInput(filter->GetOutput());
+    caster->ReleaseDataFlagOn();
+    caster->InPlaceOn();
+    
     if(noSDI){
 	FilterWatcher watcher1(filter);
-	// filter->AddObserver(itk::ProgressEvent(), eventCallbackITK);
-	// filter->AddObserver(itk::IterationEvent(), eventCallbackITK);
-	// filter->AddObserver(itk::EndEvent(), eventCallbackITK);
 	try{
 	    filter->Update();
 	    }
@@ -92,7 +82,7 @@ int DoIt(int argc, char *argv[]){
 	}
 
 
-    const typename OutputImageType::Pointer& output= filterXYZ->GetOutput();
+    const typename OutputImageType::Pointer& output= caster->GetOutput();
 
     typedef itk::ImageFileWriter<OutputImageType>  WriterType;
     typename WriterType::Pointer writer = WriterType::New();
@@ -150,18 +140,6 @@ int dispatch_pT(itk::ImageIOBase::IOPixelType pixelType, size_t dimensionType, i
     switch (pixelType){
     case itk::ImageIOBase::SCALAR:{ // 1 component per pixel
         typedef InputComponentType InputPixelType;
-        res= dispatch_D<InputComponentType, InputPixelType>(dimensionType, argc, argv);
-        } break;
-    case itk::ImageIOBase::COMPLEX:{ // 2 components per pixel
-        typedef std::complex<InputComponentType> InputPixelType;
-        res= dispatch_D<InputComponentType, InputPixelType>(dimensionType, argc, argv);
-        } break;
-    case itk::ImageIOBase::RGB:{ // 3 components per pixel, limited [0,1]
-        typedef itk::RGBPixel<InputComponentType> InputPixelType;
-        res= dispatch_D<InputComponentType, InputPixelType>(dimensionType, argc, argv);
-        } break;
-    case itk::ImageIOBase::RGBA:{ // 4 components per pixel, limited [0,1]
-        typedef itk::RGBAPixel<InputComponentType> InputPixelType;
         res= dispatch_D<InputComponentType, InputPixelType>(dimensionType, argc, argv);
         } break;
     case itk::ImageIOBase::UNKNOWNPIXELTYPE:
@@ -262,12 +240,14 @@ void GetImageType (std::string fileName,
 
 
 int main(int argc, char *argv[]){
-    if ( argc != 4 ){
+    if ( argc != 6 ){
         std::cerr << "Missing Parameters: "
                   << argv[0]
                   << " Input_Image"
                   << " Output_Image"
                   << " compress|stream-chunks"
+                  << " lower"
+                  << " upper"
                   << std::endl;
 
         std::cerr << std::endl;
